368267a5aecf798c49bf6bb22e388ecef3becc32
Marius Glior <marius.gligor@gmail.com>
2021-02-24 23:32:13 +0200

    code review

--- src/main/scala/scalable/solutions/FactorialApp.scala
+++ src/main/scala/scalable/solutions/FactorialApp.scala
@@ -4,16 +4,30 @@ import scala.annotation.tailrec
 
 object FactorialApp extends App {
 
-  def factorial(n: Int, product: BigInt): Trampoline[BigInt] = {
-    if (n <= 2) Done(product) else More(() => factorial(n - 1, n * product))
-  }
+  def factorial(n: Int, product: BigInt): Trampoline[BigInt] =
+    if (n < 2) Done(product) else More(() => factorial(n - 1, n * product))
 
   @tailrec
-  def fact(n: Int, product: BigInt): BigInt = {
-    if (n <= 2) product else fact(n - 1, n * product)
-  }
+  def fact(n: Int, product: BigInt): BigInt =
+    if (n < 2) product else fact(n - 1, n * product)
 
-  val n = 100000
-  println(factorial(n, 1).runT)
-  println(fact(n, 1))
+  // an interesting factorial calculation
+  def factorial(n: BigInt): BigInt =
+    (BigInt(1) to n).product
+
+  def factorial2(n: BigInt): BigInt =
+    (BigInt(1) to n).foldLeft(BigInt(1))((f, v) => f * v)
+
+  val n = 1000
+  val f1 = factorial(n, 1).runT
+  val f2 = fact(n, 1)
+  val f3 = factorial(n)
+  val f4 = factorial2(n)
+
+  println(f1)
+  println(f2)
+  println(f3)
+  println(f4)
+
+  assert(f1 == f2 && f1 == f3 && f1 == f4)
 }
